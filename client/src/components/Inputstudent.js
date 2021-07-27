77739e24c74a48d4c14daae4ac110c23c3e304d5
LAPTOP-0M00PN3C\Ali Iktider Sayam <aisayam.sayam@gmail.com>
2021-07-28 02:57:47 +0600

    input fields data entry with set state

--- client/src/components/Inputstudent.js
+++ client/src/components/Inputstudent.js
@@ -6,15 +6,20 @@ class Inputstudent extends React.Component{
           lastName : "",
           place : ""
      }
+     handleChange = (e) => {
+          // console.log(e.target.name);
+          // console.log(e.target.value);
+          this.setState({[e.target.name]:e.target.value});
+     }
      render(){
           return (
                <div className="row text-center">
                     <div className="col-md-4">
                          <form>
-                              <input placeholder="First Name" className="form-control"/>
-                              <input placeholder="Second Name" className="form-control"/>
-                              <input placeholder="Place" className="form-control"/>
-                              <button className="btn btn-primary">Create</button>
+                              <input onChange={(e) => this.handleChange(e)} name='firstName' value={this.state.firstName} style={{marginLeft: '50px', marginTop: '20px', fontSize: '14px', fontFamily: 'cursive, sans-serif, gugi', borderRadius: '10px'}} placeholder="First Name" className="form-control"/>
+                              <input onChange={(e) => this.handleChange(e)} name='lastName'  value={this.state.lastName} style={{marginLeft: '50px', marginTop: '20px', fontSize: '14px', fontFamily: 'cursive, sans-serif, gugi', borderRadius: '10px'}} placeholder="Second Name" className="form-control"/>
+                              <input onChange={(e) => this.handleChange(e)} name='place' value={this.state.place} style={{marginLeft: '50px', marginTop: '20px', fontSize: '14px', fontFamily: 'cursive, sans-serif, gugi', borderRadius: '10px'}} placeholder="Place" className="form-control"/>
+                              <button style={{marginLeft: '50px',marginTop: '20px',width: '430px', backgroundColor:'#4D10BE', color: 'white', fontSize: '18px', fontFamily: 'cursive, sans-serif, gugi', outline: 'none', borderRadius: '10px'}} className="btn">CREATE</button>
                          </form>
                     </div>
                     <div>
