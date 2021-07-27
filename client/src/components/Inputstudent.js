031231a4824f289d2d58143da3306ecd7cc71343
LAPTOP-0M00PN3C\Ali Iktider Sayam <aisayam.sayam@gmail.com>
2021-07-28 03:07:32 +0600

    posting data

--- client/src/components/Inputstudent.js
+++ client/src/components/Inputstudent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Inputstudent extends React.Component{
      state = {
@@ -11,14 +12,22 @@ class Inputstudent extends React.Component{
           // console.log(e.target.value);
           this.setState({[e.target.name]:e.target.value});
      }
+     handleSubmit = () => {
+          if (this.state.firstName !== '' && this.state.lastName !== '' && this.state.place !== '') {
+               // axios
+               //   .post("/")
+               //   .then(res => )
+               //   .catch(err => console.error(err));
+          }
+     }
      render(){
           return (
                <div className="row text-center">
                     <div className="col-md-4">
-                         <form>
-                              <input onChange={(e) => this.handleChange(e)} name='firstName' value={this.state.firstName} style={{marginLeft: '50px', marginTop: '20px', fontSize: '14px', fontFamily: 'cursive, sans-serif, gugi', borderRadius: '10px'}} placeholder="First Name" className="form-control"/>
-                              <input onChange={(e) => this.handleChange(e)} name='lastName'  value={this.state.lastName} style={{marginLeft: '50px', marginTop: '20px', fontSize: '14px', fontFamily: 'cursive, sans-serif, gugi', borderRadius: '10px'}} placeholder="Second Name" className="form-control"/>
-                              <input onChange={(e) => this.handleChange(e)} name='place' value={this.state.place} style={{marginLeft: '50px', marginTop: '20px', fontSize: '14px', fontFamily: 'cursive, sans-serif, gugi', borderRadius: '10px'}} placeholder="Place" className="form-control"/>
+                         <form onSubmit={() => this.handleSubmit()} >
+                              <input required onChange={(e) => this.handleChange(e)} name='firstName' value={this.state.firstName} style={{marginLeft: '50px', marginTop: '20px', fontSize: '14px', fontFamily: 'cursive, sans-serif, gugi', borderRadius: '10px'}} placeholder="First Name" className="form-control"/>
+                              <input required onChange={(e) => this.handleChange(e)} name='lastName'  value={this.state.lastName} style={{marginLeft: '50px', marginTop: '20px', fontSize: '14px', fontFamily: 'cursive, sans-serif, gugi', borderRadius: '10px'}} placeholder="Second Name" className="form-control"/>
+                              <input required onChange={(e) => this.handleChange(e)} name='place' value={this.state.place} style={{marginLeft: '50px', marginTop: '20px', fontSize: '14px', fontFamily: 'cursive, sans-serif, gugi', borderRadius: '10px'}} placeholder="Place" className="form-control"/>
                               <button style={{marginLeft: '50px',marginTop: '20px',width: '430px', backgroundColor:'#4D10BE', color: 'white', fontSize: '18px', fontFamily: 'cursive, sans-serif, gugi', outline: 'none', borderRadius: '10px'}} className="btn">CREATE</button>
                          </form>
                     </div>
